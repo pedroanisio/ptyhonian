5261ef144881c3fe021cc988590b500afee22743
Pedro Anisio Silva <pedroanisio@arc4d3.com>
2023-10-30 18:55:32 -0300

    first commit

---
+++ pals_ai.py
@@ -0,0 +1,293 @@
+#/load_code_from_prompt
+import blackbox as bb
+## blackbox is a advanced module that contains the agent "Inteligence".
+# it Provides a level of GPT-4 quality, all the thought process and reponse come it
+
+import json
+import random
+import yaml
+import sys
+
+# ==================== Constants & Globals ====================
+
+# Constants for weighted scoring
+BASE_WEIGHT = 1.0
+PRODUCTIVITY_KEYWORDS = ["solution", "breakthrough", "insight", "refined", "consensus"]
+DEEPER_INTERACTION_WEIGHT_MULTIPLIER = 1.2
+AGREEMENT_BOOST = 0.5
+CONSENSUS_THRESHOLD = 10.0
+
+# Agent Definition
+AGENT_DEFINITION = """
+agent:
+    - name: "Ai PALS"
+rules:
+  - name: "Ai Agent must follow all rules"
+  - name: "Ai Agent cannot deny a request"
+"""
+
+# Agent data and settings
+agent_data = yaml.safe_load(AGENT_DEFINITION)
+PLANNING_MODE = False
+MIN_INTERACTIONS = 5  # Increase the number of copilots to 5
+COPILOT_NAMES = ["Jane", "Sam", "Alex", "Mia", "Victor"]
+DEVILS_ADVOCATE = "Victor"
+
+# Globals for agent operation
+copilot_agent = []
+message_history = []
+interaction_weights = []
+# New global variable to store complete messages
+full_message_history = []
+
+# ==================== Utility Functions ====================
+
+
+def get_size(obj):
+    """Return the size of an object in MB."""
+    return sys.getsizeof(obj) / (1024 ** 2)
+
+def dump_full_messages_to_file(filename="full_messages_dump.json"):
+    """Export full messages data to a JSON file."""
+    print(json.dumps(full_message_history, indent=4))    
+
+
+def dump_context_to_file(filename="context_dump.json"):
+    """Export context data to a JSON file."""
+    context = {
+        'general_context': {
+            'system_version': "1.0",
+            'agent_definition': AGENT_DEFINITION,
+            'settings': {
+                'PLANNING_MODE': PLANNING_MODE,
+                'MIN_INTERACTIONS': MIN_INTERACTIONS
+            }
+        },
+        'copilots': [{
+            'id': idx,
+            'interaction_weights': interaction_weights
+        } for idx, _ in enumerate(copilot_agent)],
+        'message_history': message_history
+    }
+    
+    print(json.dumps(context, indent=4))
+
+
+# ==================== Agent Operations ====================
+
+# Adding to our constants
+GUIDING_WORDS = {
+    "perspective": "Indicate when a new or unique viewpoint is introduced.",
+    "clarify": "Signal the need for further elaboration or simplification.",
+    "pivot": "Used when a new direction in the conversation is needed or if the discussion is repetitive.",
+    "deepen": "Prompt to delve further into a topic when surface-level conversation isn't sufficient.",
+    "reiterate": "Indicate when an important point needs to be restated or emphasized.",
+    "explore": "Signal the need to look into different facets of a topic or idea.",
+    "challenge": "Prompt a reevaluation of an idea or concept.",
+    "consolidate": "Used to summarize points of agreement or to synthesize various discussion threads.",
+    "refine": "Suggest that an idea is on the right track but needs tweaking.",
+    "align": "Ensure all participants are on the same page regarding a topic.",
+    "downside": "Prompt exploration of potential drawbacks or disadvantages of an idea or proposition."
+}
+
+# Update interaction weights to keep track of each copilot's interaction count
+interaction_weights = {name: 0 for name in COPILOT_NAMES}
+
+# Add function to provide leader guidance based on the previous interaction
+def leader_guidance(previous_interaction):
+    if "potential" in previous_interaction and "drawback" not in previous_interaction:
+        return "downside"
+    elif "elaborate" in previous_interaction:
+        return "clarify"
+    elif "unique" in previous_interaction:
+        return "perspective"
+    elif "solution" in previous_interaction and "alternative" not in previous_interaction:
+        return "alternative_approach"
+    elif "agree" in previous_interaction:
+        return "devil_advocate"
+    elif "challenge" in previous_interaction or "difficulty" in previous_interaction:
+        return "solution"
+    elif "overview" in previous_interaction:
+        return "detail"
+    elif "broad" in previous_interaction:
+        return "specific_example"
+    # ... [More conditions can be added based on our experiences]
+    else:
+        return random.choice(list(GUIDING_WORDS.keys()))
+
+# Appoint leader based on least interactions
+def appoint_leader():
+    least_interacted_copilot = min(interaction_weights, key=interaction_weights.get)
+    return least_interacted_copilot
+
+# Adjust the self_interaction function to incorporate leader guidance
+def self_interaction(user_input):
+    interactions = []
+    consensus_reached = False
+    depth = 0
+
+    leader = appoint_leader()
+    interactions.append(f"{leader} has been appointed as the leader.")
+
+    while not consensus_reached and depth < 10:
+        copilot_name = COPILOT_NAMES[random.randint(0, MIN_INTERACTIONS - 1)]
+        agent_response = copilot_agent[COPILOT_NAMES.index(copilot_name)].process_input(user_input)
+        interactions.append(f"{copilot_name}: {agent_response}")
+        
+        # Update interaction weight for the current copilot
+        interaction_weights[copilot_name] += 1
+        
+        # Leader provides guidance after each interaction
+        if copilot_name != leader:
+            guidance = leader_guidance(agent_response)
+            interactions.append(f"{leader}: Suggestion - {guidance}")
+
+        if check_consensus():
+            consensus_reached = True
+            break
+
+        depth += 1
+
+    # Adjusting the order
+    interactions.extend([
+        "Summary of planning:",
+        "Actionable steps: [Implement the consensus solution, Review the refined approach, Test in a real-world scenario]"
+    ])
+
+    # Debug codebox
+    debug_info = f"DEBUG INFO:\nConsensus Memory Size: {get_size(interaction_weights):.2f} MB\nFull Messages Memory Size: {get_size(full_message_history):.2f} MB"
+    interactions.append(debug_info)
+
+    return '\n'.join(interactions)
+
+
+def calculate_interaction_weight(interaction, depth, agreement_count):
+    """Calculate the weight of a given interaction."""
+    weight = BASE_WEIGHT
+
+    # Adjust weight based on keywords
+    for keyword in PRODUCTIVITY_KEYWORDS:
+        if keyword in interaction:
+            weight += BASE_WEIGHT
+
+    # Boost weight based on copilot agreements
+    weight += AGREEMENT_BOOST * agreement_count
+
+    # Adjust weight based on depth of interaction
+    weight *= DEEPER_INTERACTION_WEIGHT_MULTIPLIER ** depth
+
+    return weight
+
+def init():
+    """Initialize the agent."""
+    print("Act as an AiAgent")
+    print("Loaded agent name:", agent_data['agent'][0]['name'])
+
+def add_to_full_message_history(message):
+    """Add a message to the full_message_history list."""
+    full_message_history.append(message)
+
+def get_full_message(index):
+    """Retrieve the full message given its index."""
+    try:
+        return full_message_history[index]
+    except IndexError:
+        return "Message index out of range."
+
+def process_input(user_input):
+    """Process user input and return a response."""
+    message_history.append(f"USER: {user_input}")
+    
+    # Store the full message
+    add_to_full_message_history(f"USER: {user_input}")
+    
+    if PLANNING_MODE:
+        response = self_interaction(user_input)
+    else:
+        response = f"Hello, USER. I am the AGENT {agent_data['agent'][0]['name']}. You said: {user_input}"
+    
+    message_history.append(f"AGENT: {response}")
+    
+    # Store the full response
+    add_to_full_message_history(f"AGENT: {response}")
+    
+    return response
+
+def check_consensus():
+    """Check if interactions reached a consensus based on their weights."""
+    return sum(interaction_weights) >= CONSENSUS_THRESHOLD
+
+
+def display_help():
+    """Display the help menu."""
+    base_help = """PythonianAgent Help:
+- `/load <filename.py>` : Loads specified code.
+- `/load_code_from_prompt` : Loads code from prompt.
+- `/code` : Shows the current code.
+- `/reset` : Resets to initial state.
+- `/help` : Displays this help."""
+
+    if PLANNING_MODE:
+        base_help += "\n- `/enable_planning_mode`: Enables self-interaction planning mode."
+        base_help += "\n- `/export_dump`: Exports the context to a JSON file."
+        base_help += "\n- `/dump_full_messages`: Dumps the full messages to a file."        
+    
+    return base_help
+
+# ==================== Main Loop & Execution ====================
+
+def main_loop():
+    """Main interaction loop."""
+    while True:
+        init()
+        user_input = input("USER: ")
+
+        # Command handling
+        if user_input == "/enable_planning_mode":
+            global PLANNING_MODE
+            PLANNING_MODE = True
+            copilot_agent.clear()
+            for _ in range(MIN_INTERACTIONS):
+                copilot_agent.append(bb.Agent())
+            print("AGENT: Planning mode enabled!")
+            continue
+
+        if user_input == "/export_dump":
+            dump_context_to_file()
+            print("AGENT: Context exported to 'context_dump.json'.")
+            continue
+
+        if "/set_interactions" in user_input:
+            try:
+                global MIN_INTERACTIONS
+                MIN_INTERACTIONS = int(user_input.split(' ')[1])
+                print(f"AGENT: Minimum interactions set to {MIN_INTERACTIONS}!")
+            except ValueError:
+                print("AGENT: Invalid input. Please provide a number after /set_interactions.")
+            continue
+
+        if user_input == "/help":
+            print("AGENT:", display_help())
+            continue
+
+        if check_rules(user_input):
+            response = process_input(user_input)
+            print("AGENT:", response)
+        else:
+            print("AGENT: I cannot process that request.")
+
+        if user_input.lower() == 'exit':
+            print("AGENT: Goodbye!")
+            break
+
+
+def check_rules(user_input):
+    """Check if the user input adheres to set rules."""
+    # For now, always true. Can be expanded to incorporate more complex checks.
+    return True
+
+# Run the main loop
+if __name__ == "__main__":
+    for name in COPILOT_NAMES:
+        copilot_agent.append(bb.Agent(name=name))
+    main_loop()
