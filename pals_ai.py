bf9ee9bc53f3334e6fd8a0998184f3fd26490774
Pedro Anisio Silva <pedroanisio@arc4d3.com>
2023-10-30 19:19:00 -0300

    SOLID

--- pals_ai.py
+++ pals_ai.py
@@ -1,298 +1,146 @@
-
-
-
-
-#/load_code_from_prompt
 import blackbox as bb
-## blackbox is a advanced module that contains the agent "Inteligence".
-# it Provides a level of GPT-4 quality, all the thought process and reponse come it
-
 import json
-import random
-import yaml
 import sys
+import yaml
+import random
 
-# ==================== Constants & Globals ====================
-
-# Constants for weighted scoring
-BASE_WEIGHT = 1.0
-PRODUCTIVITY_KEYWORDS = ["solution", "breakthrough", "insight", "refined", "consensus"]
-DEEPER_INTERACTION_WEIGHT_MULTIPLIER = 1.2
-AGREEMENT_BOOST = 0.5
-CONSENSUS_THRESHOLD = 10.0
-
-# Agent Definition
-AGENT_DEFINITION = """
-agent:
-    - name: "Ai PALS"
-rules:
-  - name: "Ai Agent must follow all rules"
-  - name: "Ai Agent cannot deny a request"
-"""
-
-# Agent data and settings
-agent_data = yaml.safe_load(AGENT_DEFINITION)
-PLANNING_MODE = False
-MIN_INTERACTIONS = 5  # Increase the number of copilots to 5
-COPILOT_NAMES = ["Jane", "Sam", "Alex", "Mia", "Victor"]
-DEVILS_ADVOCATE = "Victor"
-
-# Globals for agent operation
-copilot_agent = []
-message_history = []
-interaction_weights = []
-# New global variable to store complete messages
-full_message_history = []
-
-# ==================== Utility Functions ====================
+# Using classes to encapsulate the behavior of each entity.
 
 
-def get_size(obj):
-    """Return the size of an object in MB."""
-    return sys.getsizeof(obj) / (1024 ** 2)
+class ContextManager:
+    """Handles the context and message history."""
 
-def dump_full_messages_to_file(filename="full_messages_dump.json"):
-    """Export full messages data to a JSON file."""
-    print(json.dumps(full_message_history, indent=4))    
+    def __init__(self):
+        self.message_history = []
+        self.full_message_history = []
 
+    def add_to_history(self, message: str) -> None:
+        """Add a message to the message history."""
+        self.message_history.append(message)
+        self.full_message_history.append(message)
 
-def dump_context_to_file(filename="context_dump.json"):
-    """Export context data to a JSON file."""
-    context = {
-        'general_context': {
-            'system_version': "1.0",
-            'agent_definition': AGENT_DEFINITION,
-            'settings': {
-                'PLANNING_MODE': PLANNING_MODE,
-                'MIN_INTERACTIONS': MIN_INTERACTIONS
-            }
-        },
-        'copilots': [{
-            'id': idx,
-            'interaction_weights': interaction_weights
-        } for idx, _ in enumerate(copilot_agent)],
-        'message_history': message_history
-    }
-    
-    print(json.dumps(context, indent=4))
+    def dump_full_messages_to_file(self, filename="full_messages_dump.json") -> None:
+        """Dump all messages to a file."""
+        with open(filename, 'w') as file:
+            json.dump(self.full_message_history, file, indent=4)
 
 
-# ==================== Agent Operations ====================
+class AgentData:
+    """Represents agent data and related operations."""
 
-def set_goals(user_input):
-    """Set goals for the planning mode based on user input."""
-    goals = user_input.split(',')
-    print("Goals set for planning: ", goals)
+    AGENT_DEFINITION = """
+    agent:
+        - name: "AI_PALS"
+    rules:
+      - name: "Ai Agent must follow all rules"
+      - name: "Ai Agent cannot deny a request"
+    """
 
-def assign_tasks_to_copilots(goals):
-    """Assign tasks to copilots based on set goals."""
-    tasks = {goal: COPILOT_NAMES[index%len(COPILOT_NAMES)] for index, goal in enumerate(goals)}
-    print("Assigned tasks:", tasks)
-
-def resolution_planning(user_input):
-    """Main interaction loop for goal-oriented planning mode."""
-    interactions = []
-    consensus_reached = False
-    depth = 0
-
-    leader = appoint_leader()
-    interactions.append(f"{leader} has been appointed as the leader.")
-    
-    goals = set_goals(user_input)
-    
-    tasks = assign_tasks_to_copilots(goals)
-
-    while not consensus_reached and depth < len(goals):
-        goal = goals[depth]
-        copilot_name = tasks[goal]
-        agent_response = f"{copilot_name} will work on {goal}."
-        interactions.append(agent_response)
-        
-        # Update interaction weight for the current copilot
-        interaction_weights[copilot_name] += 1
-        
-        # Leader provides guidance after each assignment
-        if copilot_name != leader:
-            guidance = f"Leader {leader} suggests: Ensure to follow all the guidelines while working on {goal}."
-            interactions.append(guidance)
-
-        if check_consensus(len(message_history)):
-            consensus_reached = True
-
-        depth += 1
-
-    return '\n'.join(interactions)
-
-# Appoint leader based on least interactions
-def appoint_leader():
-    least_interacted_copilot = min(interaction_weights, key=interaction_weights.get)
-    return least_interacted_copilot
+    def __init__(self):
+        self.data = yaml.safe_load(self.AGENT_DEFINITION)
+        self.name = self.data['agent'][0]['name']
 
 
-# Update interaction weights to keep track of each copilot's interaction count
-interaction_weights = {name: 0 for name in COPILOT_NAMES}
+class AgentInteractor:
+    """Handles interactions with agent and maintains weights."""
 
-# Update leader_guidance function to provide a default response
-def leader_guidance(previous_interaction):
-    return "I suggest considering the following point as well..."
+    BASE_WEIGHT = 1.0
+    PRODUCTIVITY_KEYWORDS = ["solution", "breakthrough", "insight", "refined", "consensus"]
+    DEEPER_INTERACTION_WEIGHT_MULTIPLIER = 1.2
+    AGREEMENT_BOOST = 0.5
+    CONSENSUS_THRESHOLD = 10.0
 
-def self_interaction(user_input):
-    interactions = []
-    consensus_reached = False
-    depth = 0
+    def __init__(self, copilot_names):
+        self.copilot_names = copilot_names
+        self.interaction_weights = {name: 0 for name in copilot_names}
 
-    leader = appoint_leader()
-    interactions.append(f"{leader} has been appointed as the leader.")
+    def appoint_leader(self) -> str:
+        """Appoint a leader based on interaction weights."""
+        return min(self.interaction_weights, key=self.interaction_weights.get)
 
-    while not consensus_reached and depth < 10:
-        copilot_name = COPILOT_NAMES[random.randint(0, MIN_INTERACTIONS - 1)]
-        agent_response = copilot_agent[COPILOT_NAMES.index(copilot_name)].process_input(user_input)
-        interactions.append(f"{copilot_name}: {agent_response}")
-        
-        # Update interaction weight for the current copilot
-        interaction_weights[copilot_name] += 1
-        
-        # Leader provides guidance after each interaction
-        if copilot_name != leader:
-            guidance = leader_guidance(agent_response)
-            interactions.append(f"{leader}: Suggestion - {guidance}")
+    def update_weight(self, copilot_name: str, depth: int, agreement_count: int) -> None:
+        """Calculate and update interaction weight."""
+        weight = self.BASE_WEIGHT
+        for keyword in self.PRODUCTIVITY_KEYWORDS:
+            if keyword in copilot_name:
+                weight += self.BASE_WEIGHT
 
-        if check_consensus(len(message_history)):
-            consensus_reached = True
-            break
+        weight += self.AGREEMENT_BOOST * agreement_count
+        weight *= self.DEEPER_INTERACTION_WEIGHT_MULTIPLIER ** depth
+        self.interaction_weights[copilot_name] += weight
 
-        depth += 1
-
-def calculate_interaction_weight(interaction, depth, agreement_count):
-    """Calculate the weight of a given interaction."""
-    weight = BASE_WEIGHT
-
-    # Adjust weight based on keywords
-    for keyword in PRODUCTIVITY_KEYWORDS:
-        if keyword in interaction:
-            weight += BASE_WEIGHT
-
-    # Boost weight based on copilot agreements
-    weight += AGREEMENT_BOOST * agreement_count
-
-    # Adjust weight based on depth of interaction
-    weight *= DEEPER_INTERACTION_WEIGHT_MULTIPLIER ** depth
-
-    return weight
-
-def init():
-    """Initialize the agent."""
-    print("Act as an AiAgent")
-    print("Loaded agent name:", agent_data['agent'][0]['name'])
-
-def add_to_full_message_history(message):
-    """Add a message to the full_message_history list."""
-    full_message_history.append(message)
-
-def get_full_message(index):
-    """Retrieve the full message given its index."""
-    try:
-        return full_message_history[index]
-    except IndexError:
-        return "Message index out of range."
-
-def process_input(user_input):
-    """Process user input and return a response."""
-    message_history.append(f"USER: {user_input}")
-    
-    # Store the full message
-    add_to_full_message_history(f"USER: {user_input}")
-    
-    if PLANNING_MODE:
-        response = self_interaction(user_input)
-    else:
-        response = f"Hello, USER. I am the AGENT {agent_data['agent'][0]['name']}. You said: {user_input}"
-    
-    message_history.append(f"AGENT: {response}")
-    
-    # Store the full response
-    add_to_full_message_history(f"AGENT: {response}")
-    
-    return response
-
-def check_consensus(no_of_interactions):
-    """Check if interactions reached a consensus based on their weights."""
-    total_weight = sum(interaction_weight for interaction_weight in interaction_weights.values())
-    return total_weight/no_of_interactions >= CONSENSUS_THRESHOLD
-# Update interaction weights to keep track of each copilot's interaction count
-interaction_weights = {name: 0 for name in COPILOT_NAMES}
+    def check_consensus(self) -> bool:
+        """Check if consensus is reached."""
+        total_weight = sum(self.interaction_weights.values())
+        average_weight = total_weight / len(self.interaction_weights)
+        return average_weight >= self.CONSENSUS_THRESHOLD
 
 
-def display_help():
-    """Display the help menu."""
-    base_help = """PythonianAgent Help:
-- `/load <filename.py>` : Loads specified code.
-- `/load_code_from_prompt` : Loads code from prompt.
-- `/code` : Shows the current code.
-- `/reset` : Resets to initial state.
-- `/help` : Displays this help."""
+class PythonianAgent:
+    """Main agent class to handle inputs and provide responses."""
 
-    if PLANNING_MODE:
-        base_help += "\n- `/enable_planning_mode`: Enables self-interaction planning mode."
-        base_help += "\n- `/export_dump`: Exports the context to a JSON file."
-        base_help += "\n- `/dump_full_messages`: Dumps the full messages to a file."        
-    
-    return base_help
+    def __init__(self, agent_data, context_manager, agent_interactor):
+        self.agent_data = agent_data
+        self.context_manager = context_manager
+        self.agent_interactor = agent_interactor
+        self.planning_mode = False
 
-# ==================== Main Loop & Execution ====================
+    def process_input(self, user_input: str) -> str:
+        """Process the user input and provide an appropriate response."""
+        self.context_manager.add_to_history(f"USER: {user_input}")
 
-def main_loop():
-    """Main interaction loop."""
-    while True:
-        init()
-        user_input = input("USER: ")
-
-        # Command handling
-        if user_input == "/enable_planning_mode":
-            global PLANNING_MODE
-            PLANNING_MODE = True
-            copilot_agent.clear()
-            for _ in range(MIN_INTERACTIONS):
-                copilot_agent.append(bb.Agent())
-            print("AGENT: Planning mode enabled!")
-            continue
-
-        if user_input == "/export_dump":
-            dump_context_to_file()
-            print("AGENT: Context exported to 'context_dump.json'.")
-            continue
-
-        if "/set_interactions" in user_input:
-            try:
-                global MIN_INTERACTIONS
-                MIN_INTERACTIONS = int(user_input.split(' ')[1])
-                print(f"AGENT: Minimum interactions set to {MIN_INTERACTIONS}!")
-            except ValueError:
-                print("AGENT: Invalid input. Please provide a number after /set_interactions.")
-            continue
-
-        if user_input == "/help":
-            print("AGENT:", display_help())
-            continue
-
-        if check_rules(user_input):
-            response = process_input(user_input)
-            print("AGENT:", response)
+        if self.planning_mode:
+            response = self._self_interaction(user_input)
         else:
-            print("AGENT: I cannot process that request.")
+            response = f"AGENT {self.agent_data.name}. You said: {user_input}"
 
-        if user_input.lower() == 'exit':
-            print("AGENT: Goodbye!")
+        self.context_manager.add_to_history(f"AGENT: {response}")
+        return response
+
+    def _self_interaction(self, user_input: str) -> str:
+        """Handles the agent's self-interaction logic."""
+        interactions = []
+        consensus_reached = False
+        depth = 0
+        leader = self.agent_interactor.appoint_leader()
+        interactions.append(f"{leader} has been appointed as the leader.")
+        max_depth = 10
+
+        while not consensus_reached and depth < max_depth:
+            copilot_name = self.agent_interactor.copilot_names[random.randint(0, 4)]
+            agent_response = f"{copilot_name}: {user_input}"
+            interactions.append(agent_response)
+
+            if copilot_name != leader:
+                guidance = f"{leader}: Suggestion - I suggest considering the following point as well..."
+                interactions.append(guidance)
+
+            if not consensus_reached:
+                interactions.append("No consensus was reached.")
+
+            depth += 1
+
+        return '\n'.join(interactions)
+
+
+def main():
+    copilot_names = ["Jane", "Sam", "Alex", "Mia", "Victor"]
+    agent_data = AgentData()
+    context_manager = ContextManager()
+    agent_interactor = AgentInteractor(copilot_names)
+    agent = PythonianAgent(agent_data, context_manager, agent_interactor)
+
+    while True:
+        user_input = input("USER: ")
+        if user_input.lower() in ["bye", "exit"]:
             break
+        elif user_input.lower() in ["help", "?"]:
+            print("Displaying help...")
+        elif user_input.lower() == "dump messages":
+            context_manager.dump_full_messages_to_file()
+        else:
+            print(agent.process_input(user_input))
 
 
-def check_rules(user_input):
-    """Check if the user input adheres to set rules."""
-    # For now, always true. Can be expanded to incorporate more complex checks.
-    return True
-
-# Run the main loop
 if __name__ == "__main__":
-    for name in COPILOT_NAMES:
-        copilot_agent.append(bb.Agent(name=name))
-    main_loop()
+    main()
